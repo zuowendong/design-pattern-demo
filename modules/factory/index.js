d967f3c31302df8c5e1095e4a94816640d4dae67
zuowendong <17326195691@163.com>
2023-01-07 15:06:46 +0800

    模态框基本实现

--- modules/factory/index.js
+++ modules/factory/index.js
@@ -1,1 +1,38 @@
-console.log(123);
+(() => {
+	const oModal = document.getElementsByClassName("modal")[0];
+	const oBtnGroup = document.getElementsByClassName("btn-group")[0];
+
+	const init = () => {
+		bindEvent();
+	};
+
+	function bindEvent() {
+		oBtnGroup.addEventListener("click", handleBtnClick, false);
+	}
+
+	function handleBtnClick(e) {
+		const tar = e.target;
+		const tagName = tar.tagName.toLowerCase();
+		if (tagName === "button") {
+			const status = tar.dataset.status;
+			changeStatus(status);
+		}
+	}
+	function changeStatus(status) {
+		switch (status) {
+			case "S":
+				oModal.className = "modal success";
+				break;
+			case "W":
+				oModal.className = "modal warning";
+				break;
+			case "E":
+				oModal.className = "modal error";
+				break;
+			default:
+				break;
+		}
+	}
+
+	init();
+})();
